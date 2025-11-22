97b15204a54e511701f2674ff5903fc963ad03df
Puzzlesla <fatimabala@icloud.com>
2025-11-22 10:21:31 -0500

    Added second and last page and updated style.css

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,3 +8,22 @@ def home():
 
 with app.test_request_context():
     print(url_for("static", filename="styles.css"))
+
+
+@app.route("/home1", methods=["GET", "POST"])
+def home1():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("task")
+    return render_template("home1.html", name=name)
+
+@app.route("/simonsays/")
+def simonsays():
+    return render_template("simonsays.html")
+
+with app.test_request_context():
+    print(url_for("static", filename="simonsays.css"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
